ee12b7fb3e901f7dc3267472fb79b50e78901121
Rohan Dave <rohandewangdave@gmail.com>
2025-02-21 17:25:32 -0500

    setup for playlist collaboration

--- src/api/playlists/application/interfaces/playlist-collaboration.service.interface.ts
+++ src/api/playlists/application/interfaces/playlist-collaboration.service.interface.ts
@@ -8,34 +8,10 @@ export default interface IPlaylistCollaborationService {
   addCollaborators(id: string, collaboratorIds: string[]): Promise<void>;
 
   /**
-   * Sends a request to collaborate on a playlist.
-   * @param id - The ID of the playlist.
-   * @param collaboratorId - The ID of the user requesting collaboration.
-   * @returns A promise that resolves when the request is sent.
-   */
-  requestCollaboration(id: string, collaboratorId: string): Promise<void>;
-
-  /**
    * Removes collaborators from a playlist.
    * @param id - The ID of the playlist.
    * @param collaboratorIds - An array of user IDs to be removed as collaborators.
    * @returns A promise that resolves when collaborators are removed.
    */
   removeCollaborators(id: string, collaboratorIds: string[]): Promise<void>;
-
-  /**
-   * Accepts a collaboration request for a playlist.
-   * @param id - The ID of the playlist.
-   * @param collaboratorId - The ID of the user whose request is being accepted.
-   * @returns A promise that resolves when the collaborator is added.
-   */
-  acceptCollaborator(id: string, collaboratorId: string): Promise<void>;
-
-  /**
-   * Rejects a collaboration request for a playlist.
-   * @param id - The ID of the playlist.
-   * @param collaboratorId - The ID of the user whose request is being rejected.
-   * @returns A promise that resolves when the request is rejected.
-   */
-  rejectCollaborator(id: string, collaboratorId: string): Promise<void>;
 }
--- src/api/playlists/domain/services/playlist-collaboration.service.ts
+++ src/api/playlists/domain/services/playlist-collaboration.service.ts
@@ -6,22 +6,22 @@ export default class PlaylistCollaborationService
   implements IPlaylistCollaborationService
 {
   addCollaborators(id: string, collaboratorIds: string[]): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-
-  requestCollaboration(id: string, collaboratorId: string): Promise<void> {
+    /**
+     * Should be able to add collaborator only if they are friends
+     * Playlist should exist
+     * Requesting user should be the owner of playlist
+     * Added user should not already be a collaborator on the playlist
+     * Playlist should not exceed maximum number of collaborators
+     */
     throw new Error('Method not implemented.');
   }
 
   removeCollaborators(id: string, collaboratorIds: string[]): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-
-  acceptCollaborator(id: string, collaboratorId: string): Promise<void> {
-    throw new Error('Method not implemented.');
-  }
-
-  rejectCollaborator(id: string, collaboratorId: string): Promise<void> {
+    /**
+     * Playlist should exist
+     * User should be a collaborator on the playlist
+     * User should not be the owner of the playlist
+     */
     throw new Error('Method not implemented.');
   }
 }
