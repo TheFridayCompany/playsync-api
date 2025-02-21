7e3fef47843431927a5e43345983750551aeb1ac
Rohan Dave <rohandewangdave@gmail.com>
2025-02-21 16:57:09 -0500

    Update song.schema.ts

--- src/api/songs/data/schema/song.schema.ts
+++ src/api/songs/data/schema/song.schema.ts
@@ -3,15 +3,32 @@ import { Schema } from 'mongoose';
 export const ArtistSchema = new Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
-  web_urls: { type: [String], default: [] },
-  uris: { type: [String], default: [] },
+  web_urls: {
+    type: Map,
+    of: String,
+    default: () => ({}),
+  },
+  uris: {
+    type: Map,
+    of: String,
+    default: () => ({}),
+  },
 });
 
 export const SongSchema = new Schema({
   id: { type: String, required: true },
   name: { type: String, required: true },
   duration_ms: { type: Number, required: true },
-  web_urls: { type: [String], default: [] },
-  uris: { type: [String], default: [] },
+  web_urls: {
+    type: Map,
+    of: String,
+    default: () => ({}),
+  },
+  uris: {
+    type: Map,
+    of: String,
+    default: () => ({}),
+  },
+
   artists: { type: [ArtistSchema], default: [] }, // Embedding Artists inside Song
 });
