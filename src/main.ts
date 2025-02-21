293b504709cfdcb990651af8d27cd274324fbe6a
Rohan Dave <rohandewangdave@gmail.com>
2025-02-21 17:43:10 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { GlobalExceptionFilter } from './filters/global-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new GlobalExceptionFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
