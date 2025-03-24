6704bc8fbdf179edccbe818207105f2b2ff4241a
Rohan Dave <rohandewangdave@gmail.com>
2025-03-24 17:34:50 -0400

    cors setup

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://playsync-web-oeqop8ewx-rohandewangdaves-projects.vercel.app/',
+    ], // Specify allowed origins
+    methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Allow cookies/auth headers
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalFilters(new GlobalExceptionFilter());
-  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  await app.listen(process.env.PORT || 4000, '0.0.0.0');
 }
 bootstrap();
